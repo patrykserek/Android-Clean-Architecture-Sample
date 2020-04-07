d761e62549466690d2bd26f35ec99b88d3225878
keresmi <keresmidev@gmail.com>
2020-04-07 21:16:02 +0200

    Initial commit

---
+++ app/src/main/java/pl/akademiaandroida/android_clean_architecture_sample/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,29 @@
+package pl.akademiaandroida.android_clean_architecture_sample.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import pl.akademiaandroida.android_clean_architecture_sample.R
+
+class DashboardFragment : Fragment() {
+
+    private val dashboardViewModel by viewModels<DashboardViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
---
+++ app/src/main/java/pl/akademiaandroida/android_clean_architecture_sample/ui/notifications/NotificationsFragment.kt
@@ -0,0 +1,29 @@
+package pl.akademiaandroida.android_clean_architecture_sample.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import pl.akademiaandroida.android_clean_architecture_sample.R
+
+class NotificationsFragment : Fragment() {
+
+    private val notificationsViewModel by viewModels<NotificationsViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
+        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
