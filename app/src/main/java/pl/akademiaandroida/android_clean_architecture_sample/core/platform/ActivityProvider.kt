8d377b43a0ece8b3247361fd4ebef6eef4e7da96
keresmi <keresmidev@gmail.com>
2020-04-07 21:25:30 +0200

    Added ActivityProvider class.

---
+++ app/src/main/java/pl/akademiaandroida/android_clean_architecture_sample/core/platform/ActivityProvider.kt
@@ -0,0 +1,40 @@
+package pl.akademiaandroida.android_clean_architecture_sample.core.platform
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+
+class ActivityProvider(application: Application) {
+
+    var foregroundActivity: Activity? = null
+
+    init {
+        application.registerActivityLifecycleCallbacks(object :
+            Application.ActivityLifecycleCallbacks {
+
+            override fun onActivityPaused(activity: Activity?) {
+                foregroundActivity = null
+            }
+
+            override fun onActivityResumed(activity: Activity?) {
+                foregroundActivity = activity
+            }
+
+            override fun onActivityStarted(activity: Activity?) {
+            }
+
+            override fun onActivityDestroyed(activity: Activity?) {
+            }
+
+            override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {
+            }
+
+            override fun onActivityStopped(activity: Activity?) {
+            }
+
+            override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
+
+            }
+        })
+    }
+}
