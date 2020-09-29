ee0b4935ea7adefb7a1e70f67962262b99bccf44
keresmi <keresmidev@gmail.com>
2020-09-29 15:51:52 +0200

    Updated libraries to the latest version.

--- app/src/main/java/pl/akademiaandroida/android_clean_architecture_sample/core/provider/ActivityProvider.kt
+++ app/src/main/java/pl/akademiaandroida/android_clean_architecture_sample/core/provider/ActivityProvider.kt
@@ -12,27 +12,27 @@ class ActivityProvider(application: Application) {
         application.registerActivityLifecycleCallbacks(object :
             Application.ActivityLifecycleCallbacks {
 
-            override fun onActivityPaused(activity: Activity?) {
+            override fun onActivityPaused(activity: Activity) {
                 foregroundActivity = null
             }
 
-            override fun onActivityResumed(activity: Activity?) {
+            override fun onActivityResumed(activity: Activity) {
                 foregroundActivity = activity
             }
 
-            override fun onActivityStarted(activity: Activity?) {
+            override fun onActivityStarted(activity: Activity) {
             }
 
-            override fun onActivityDestroyed(activity: Activity?) {
+            override fun onActivityDestroyed(activity: Activity) {
             }
 
-            override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
             }
 
-            override fun onActivityStopped(activity: Activity?) {
+            override fun onActivityStopped(activity: Activity) {
             }
 
-            override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
             }
         })
     }
